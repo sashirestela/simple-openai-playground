c0d497507816bdee970d25880e086396b4baa8c6
Sashir Estela <sashirestela@gmail.com>
2025-02-06 19:31:26 +0000

    Update libraries and demos

--- src/main/java/io/github/sashirestela/openai/playground/DemoVision.java
+++ src/main/java/io/github/sashirestela/openai/playground/DemoVision.java
@@ -1,79 +1,81 @@
 package io.github.sashirestela.openai.playground;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Base64;
 import java.util.List;
 
 import io.github.sashirestela.openai.OpenAI;
 import io.github.sashirestela.openai.SimpleOpenAI;
+import io.github.sashirestela.openai.common.content.ContentPart.ContentPartImageUrl;
+import io.github.sashirestela.openai.common.content.ContentPart.ContentPartImageUrl.ImageUrl;
+import io.github.sashirestela.openai.common.content.ContentPart.ContentPartText;
+import io.github.sashirestela.openai.domain.chat.Chat;
+import io.github.sashirestela.openai.domain.chat.ChatMessage.UserMessage;
 import io.github.sashirestela.openai.domain.chat.ChatRequest;
-import io.github.sashirestela.openai.domain.chat.content.ContentPartImage;
-import io.github.sashirestela.openai.domain.chat.content.ContentPartText;
-import io.github.sashirestela.openai.domain.chat.content.ImageUrl;
-import io.github.sashirestela.openai.domain.chat.message.ChatMsgUser;
+import io.github.sashirestela.openai.support.Base64Util;
+import io.github.sashirestela.openai.support.Base64Util.MediaType;
 
 public class DemoVision {
 
     private SimpleOpenAI openai;
     private OpenAI.ChatCompletions chatService;
+    private String model;
 
     public DemoVision() {
         openai = SimpleOpenAI.builder()
                 .apiKey(System.getenv("OPENAI_API_KEY"))
                 .build();
         chatService = openai.chatCompletions();
+        model = "gpt-4o-mini";
     }
 
     public void demoCallChatWithVisionExternalImage() {
         var chatRequest = ChatRequest.builder()
-                .model("gpt-4-turbo-2024-04-09")
+                .model(model)
                 .messages(List.of(
-                        new ChatMsgUser(List.of(
-                                new ContentPartText(
+                        UserMessage.of(List.of(
+                                ContentPartText.of(
                                         "What do you see in the image? Give in details in no more than 100 words."),
-                                new ContentPartImage(new ImageUrl(
+                                ContentPartImageUrl.of(ImageUrl.of(
                                         "https://upload.wikimedia.org/wikipedia/commons/e/eb/Machu_Picchu%2C_Peru.jpg"))))))
                 .temperature(0.0)
-                .maxTokens(500)
+                .maxCompletionTokens(500)
                 .build();
         var chatResponse = chatService.createStream(chatRequest).join();
-        chatResponse.filter(chatResp -> chatResp.firstContent() != null)
-                .map(chatResp -> chatResp.firstContent())
-                .forEach(System.out::print);
+        chatResponse.forEach(this::processResponseChunk);
         System.out.println();
     }
 
     public void demoCallChatWithVisionLocalImage() {
         var chatRequest = ChatRequest.builder()
-                .model("gpt-4-turbo-2024-04-09")
+                .model(model)
                 .messages(List.of(
-                        new ChatMsgUser(List.of(
-                                new ContentPartText(
+                        UserMessage.of(List.of(
+                                ContentPartText.of(
                                         "What do you see in the image? Give in details in no more than 100 words."),
-                                new ContentPartImage(loadImageAsBase64("src/main/resources/machupicchu.jpg"))))))
+                                ContentPartImageUrl.of(ImageUrl.of(
+                                        Base64Util.encode("src/main/resources/machupicchu.jpg", MediaType.IMAGE)))))))
                 .temperature(0.0)
-                .maxTokens(500)
+                .maxCompletionTokens(500)
                 .build();
         var chatResponse = chatService.createStream(chatRequest).join();
-        chatResponse.filter(chatResp -> chatResp.firstContent() != null)
-                .map(chatResp -> chatResp.firstContent())
-                .forEach(System.out::print);
+        chatResponse.forEach(this::processResponseChunk);
         System.out.println();
     }
 
-    private ImageUrl loadImageAsBase64(String imagePath) {
-        try {
-            Path path = Paths.get(imagePath);
-            byte[] imageBytes = Files.readAllBytes(path);
-            String base64String = Base64.getEncoder().encodeToString(imageBytes);
-            var extension = imagePath.substring(imagePath.lastIndexOf('.') + 1);
-            var prefix = "data:image/" + extension + ";base64,";
-            return new ImageUrl(prefix + base64String);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+    private void processResponseChunk(Chat responseChunk) {
+        var choices = responseChunk.getChoices();
+        if (!choices.isEmpty()) {
+            var delta = choices.get(0).getMessage();
+            if (delta.getContent() != null) {
+                System.out.print(delta.getContent());
+            }
+            if (delta.getReasoningContent() != null) {
+                System.out.print(delta.getReasoningContent());
+            }
+        }
+        var usage = responseChunk.getUsage();
+        if (usage != null && usage.getCompletionTokens() != 0) {
+            System.out.println("\n");
+            System.out.println(usage);
         }
     }
 
